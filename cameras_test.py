162d5fb8ad98e19273ede6e137a8923d7a1be7d1
Owluska <kagirina@gmail.com>
2021-05-06 10:40:21 +0100

    Feature detection some bugs fixed

--- cameras_test.py
+++ cameras_test.py
@@ -31,22 +31,29 @@ while(True):
         cv2.destroyAllWindows()
         print('Error capturing camera2. Breaking loop')
         break    
+    #image resize scale in percents
+    scale = 50
+    width = int(frame1.shape[1] * scale/100)
+    height = int(frame1.shape[0] * scale/100)
+    dsize = (width, height)
+    rframe1 = cv2.resize(frame1, dsize)
+    rframe2 = cv2.resize(frame2, dsize)
     #original frame to grayscale    
-    gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-    gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+    gray1 = cv2.cvtColor(rframe1, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(rframe2, cv2.COLOR_BGR2GRAY)
     #show original frame
-    cv2.imshow('1', gray1)
-    cv2.imshow('2', gray2)
+#    cv2.imshow('1', gray1)
+#    cv2.imshow('2', gray2)
     #find the keypoints and descriptions with SIFT detector
-    kp1, des1 = orb.detectAndCompute(frame1, None)
-    kp2, des2 = orb.detectAndCompute(frame2, None)
+    kp1, des1 = orb.detectAndCompute(gray1, None)
+    kp2, des2 = orb.detectAndCompute(gray2, None)
     #create BF matcher object
     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = True)
     matches = bf.match(des1, des2)
     #sort matches in order of their distance
     matches = sorted(matches, key = lambda x:x.distance)
     #draw first ten matches
-    frame3 = cv2.drawMatches(frame1, kp1, frame2, kp2, matches[:10], None, flags=2)
+    frame3 = cv2.drawMatches(gray1, kp1, gray2, kp2, matches[:10], None, flags=2)
     cv2.imshow('matches', frame3)
     
     key = cv2.waitKey(1) & 0xFF
